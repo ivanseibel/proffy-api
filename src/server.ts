ae63f099e6cdb6182c9b271683ebe8f44bed2662
Ivan L. Seibel <ivan@pop-os.localdomain>
2020-08-04 21:11:10 -0300

    Move routes to routes.ts and implement post route.

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-interface IUser {
-  id: string;
-  name: string;
-}
-
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello World' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
